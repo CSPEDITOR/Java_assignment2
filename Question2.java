c06c02feaf68b937a3e89cda79e8232ead206014
Chandra shekhara prasad <chandrashekharaprasad@gmail.com>
2024-09-06 18:18:59 +0530

    adding word file and slovin number 2 question

---
+++ Question2.java
@@ -0,0 +1,9 @@
+// II. Verify whether difference between Summation of all even digits and 
+// odd digits of and prime digits is equal to half of sum of all digits of any 
+// number
+
+public class Question2 {
+    public static void main(String[] args) {
+        
+    }
+}
